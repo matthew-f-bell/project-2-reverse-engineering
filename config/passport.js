03f8adb5841611eca41e51beca127d8ab64c878c
xdjx3runx <xdjx3runx@gmail.com>
2022-01-19 18:56:49 -0800

    serial and deserial

--- config/passport.js
+++ config/passport.js
@@ -32,3 +32,13 @@ passport.use(
         }
     )
 );
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
