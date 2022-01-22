b5f687c15f1d8b19c4a609560aec616d429a00a6
xdjx3runx <xdjx3runx@gmail.com>
2022-01-22 06:14:35 -0800

    removed search bar, css on all jobs pages

--- routes/googleOAuth.js
+++ routes/googleOAuth.js
@@ -17,7 +17,7 @@ router.get(
 router.get(
     "/oauth2callback",
     passport.authenticate("google", {
-        successRedirect: "/users",
+        successRedirect: "/",
         failureRedirect: "/",
     })
 );
